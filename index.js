5427da76199a0b9d1d0869be954da40a4600de8b
Oleksandr Nikolaichuk <57410473+Sekator778@users.noreply.github.com>
2022-11-30 17:33:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//-----------------------------------------------------------------------------
+// NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
+//-----------------------------------------------------------------------------
+
+exports.helloWorld = (req, res) => {
+  const message="<font color='blue'>СloudFunction build Sekator + inet!</font><br><b>App Version 1.1</b>";
+  res.status(200).send(message);
+};
